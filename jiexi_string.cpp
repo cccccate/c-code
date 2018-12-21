b1d24009907ff0e47761600053570f16db1f301f
ccate <1300816522@qq.com>
2018-12-21 18:48:40 +0800

    解析字符串，计算43*56的值

---
+++ jiexi_string.cpp
@@ -0,0 +1,37 @@
+//文件名：jiexi_string
+//作  者：ccate
+//时  间：2018.12
+//描  述：解析字符串,给出字符串"43*56="，提取43，*，56并输出43*56的值
+
+#include<stdio.h>
+
+int main()
+{
+	char a[100] = "43*56=";
+	int i,j;
+	char c;
+	sscanf(a,"%d%c%d=",&i,&c,&j);
+	//printf("%d, %c, %d\n",i,c,j);
+	int res = 0;
+	switch(c)
+	{
+	case '+':
+		res = i+j;
+		break;
+	case '-':
+		res = i-j;
+		break;
+	case '*':
+		res = i*j;
+		break;
+	case '/':
+		res = i/j;
+		break;
+	default:
+		res = 0;
+	}
+	sprintf(a, "%d%c%d=%d",i,c,j,res);
+	printf("%s\n",a);
+
+	return 0;
+}
